990a3e524118880002141c146f61a96b65676b2b
abdulqad33r <93977921+abdulqad33r@users.noreply.github.com>
2026-07-29 19:37:49 +0500

    feat(auth): add token service

---
+++ server/src/services/token.service.ts
@@ -0,0 +1,35 @@
+import { sign, verify } from "hono/jwt"
+import type { SignatureAlgorithm } from "hono/utils/jwt/jwa"
+
+import env from "@/config/env"
+import type { UserRole } from "@/db/schema/enums"
+import { parseExpiry } from "@/utils/parseExpiry"
+
+export type AccessTokenPayload = {
+  sub: string
+  role: UserRole
+  sessionId: string
+  exp: number
+}
+
+const ALG: SignatureAlgorithm = "HS256"
+
+export const signAccessToken = (payload: Omit<AccessTokenPayload, "exp">) => {
+  const exp: number =
+    Math.floor(Date.now() / 1000) + parseExpiry(env.JWT_ACCESS_EXPIRES_IN)
+
+  return sign({ ...payload, exp }, env.JWT_ACCESS_SECRET, ALG)
+}
+
+export const verifyAccessToken = (token: string) =>
+  verify(token, env.JWT_ACCESS_SECRET, ALG) as Promise<AccessTokenPayload>
+
+export const generateRefreshToken = () =>
+  crypto.randomUUID() + crypto.randomUUID().replace(/-/g, "")
+
+export const hashRefreshToken = (token: string) =>
+  Bun.CryptoHasher.hash("sha256", token, "hex")
+//   const hasher = new Bun.CryptoHasher("sha256")
+//   hasher.update(token)
+//   return hasher.digest("hex")
+// }
