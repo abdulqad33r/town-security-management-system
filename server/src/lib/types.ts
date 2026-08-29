60bf1edfdf5bb281d8001868a952e68f6ca1138b
abdulqad33r <93977921+abdulqad33r@users.noreply.github.com>
2026-08-29 15:16:22 +0500

    feat(auth): add the user data type

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,9 +1,14 @@
 import type { OpenAPIHono } from "@hono/zod-openapi"
 import type { Logger } from "pino"
 
+import type { UserRole } from "@/db/schema/enums"
+
 export interface AppEnv {
   Variables: {
     logger: Logger
+    accountId: string
+    sessionId: string
+    role: UserRole
   }
 }
 
